8940974c9eb9651d6001641be6357d1e44d95549
Emilio Molina <emilio.mol.mar@gmail.com>
2018-05-06 16:08:16 +0200

    Add setup.py and needed files to upload package to pip

---
+++ audio_degrader/__init__.py
@@ -0,0 +1,14 @@
+from audio_degrader import mix_with_sound, convolve, ffmpeg
+from audio_degrader import lame, apply_gain
+from audio_degrader import apply_eq, tmp_path, remove_tmp_files
+from audio_degrader import apply_dr_compression
+
+__all__ = ["mix_with_sound",
+           "convolve",
+           "ffmpeg",
+           "lame",
+           "apply_gain",
+           "apply_eq",
+           "tmp_path",
+           "remove_tmp_files",
+           "apply_dr_compression"]
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='audio_degrader',
+    packages=['audio_degrader'],
+    version='1.0',
+    description='Tool to introduce controlled degradations to audio',
+    author='Emilio Molina',
+    author_email='emilio.mol.mar@gmail.com',
+    url='https://github.com/EliosMolina/audio_degrader',
+    download_url='https://github.com/EliosMolina/audio_degrader/archive/master.zip',
+    keywords=['audio', 'degradation', 'augmentation'],
+    install_requires=['numpy >= 1.14.3', 'librosa >= 0.6.0'],
+    package_data={'audio_degrader': ['resources/impulse_responses/*',
+                                     'resources/sounds/*']},
+    include_package_data=True
+)
